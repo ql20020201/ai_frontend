c148178b60dbdcaec6d8b7e06d6362bc252d0e33
lang qin <neale.qin@gmail.com>
2024-03-13 15:00:34 +0800

    创建比赛基本完成

--- src/pages/t_create_game.vue
+++ src/pages/t_create_game.vue
@@ -1,46 +1,64 @@
 <template>
-    <el-form ref="form" :model="form" label-width="80px">
-        <el-form-item label="活动名称">
+    <el-form ref="form" :model="form" label-width="6%" style="font-size: 16px; 
+                                                              font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                                                              font-weight: 500;
+                                                              margin: 1%; ">
+        <el-form-item label="Name" >
              <el-input v-model="form.name"></el-input>
         </el-form-item>
-        <el-form-item label="活动区域">
-            <el-select v-model="form.region" placeholder="请选择活动区域">
+        <el-form-item label="Course">
+            <el-select v-model="form.region" placeholder="select">
                 <el-option label="区域一" value="shanghai"></el-option>
                 <el-option label="区域二" value="beijing"></el-option>
             </el-select>
         </el-form-item>
-        <el-form-item label="活动时间">
-            <el-col :span="11">
-                <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+        <el-form-item label="Type">
+            <el-select v-model="form.region" placeholder="select">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item label="Time">
+            <el-col style="width: 20%;">
+                <el-date-picker type="date" placeholder="Start" v-model="form.date1" style="width: 100%;"></el-date-picker>
             </el-col>
-            <el-col class="line" :span="2">-</el-col>
-            <el-col :span="11">
-                <el-time-picker placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+            <el-col class="line" style="width: 2%;">----</el-col>
+            <el-col style="width: 20%;">
+                <el-date-picker type="date" placeholder="End" v-model="form.date2" style="width: 100%;"></el-date-picker>
             </el-col>
         </el-form-item>
-        <el-form-item label="即时配送">
-            <el-switch v-model="form.delivery"></el-switch>
+        <el-form-item label="Upload">
+            <el-upload
+                class="upload-demo"
+                action="https://jsonplaceholder.typicode.com/posts/"
+                :on-change="handleChange"
+                :file-list="fileList">
+                <el-button size="small" type="primary" style="font-size: 14px;">Click</el-button>
+            </el-upload>
         </el-form-item>
-        <el-form-item label="活动性质">
+        <el-form-item label="Tab">
             <el-checkbox-group v-model="form.type">
-                <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                <el-checkbox label="地推活动" name="type"></el-checkbox>
-                <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+                <el-checkbox label="AI" name="type"></el-checkbox>
+                <el-checkbox label="Deep learing" name="type"></el-checkbox>
+                <el-checkbox label="RNN" name="type"></el-checkbox>
+                <el-checkbox label="For new" name="type"></el-checkbox>
             </el-checkbox-group>
         </el-form-item>
-        <el-form-item label="特殊资源">
+        <el-form-item label="Level">
             <el-radio-group v-model="form.resource">
-                <el-radio label="线上品牌商赞助"></el-radio>
-                <el-radio label="线下场地免费"></el-radio>
+                <el-radio label="Easy"></el-radio>
+                <el-radio label="Medium"></el-radio>
+                <el-radio label="Hard"></el-radio>
             </el-radio-group>
         </el-form-item>
-        <el-form-item label="活动形式">
-            <el-input type="textarea" v-model="form.desc"></el-input>
+        <el-form-item label="Descripetion">
+            <el-input type="textarea" v-model="form.desc" style="font-size: 14px; 
+                                                                 font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                                                                 height: 100%;"></el-input>
         </el-form-item>
         <el-form-item>
-            <el-button type="primary" @click="onSubmit">立即创建</el-button>
-            <el-button>取消</el-button>
+            <el-button type="primary" @click="onSubmit">Submit</el-button>
+            <el-button>Cancel</el-button>
         </el-form-item>
     </el-form>
 </template>
@@ -64,7 +82,14 @@
     methods: {
       onSubmit() {
         console.log('submit!');
+      },
+      handleChange(file, fileList) {
+        this.fileList = fileList.slice(-3);
       }
     }
   }
 </script>
+
+<style scoped>
+
+</style>
